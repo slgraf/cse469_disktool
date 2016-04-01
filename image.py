431228b1f3959cab46c5af4f26508890616692ce
slgraf <slgraf@asu.edu>
2016-03-31 20:39:39 -0700

    testing

---
+++ image.py
@@ -0,0 +1,25 @@
+# project imports
+import argparse
+import os
+
+# cmd=[]
+
+# put at end of code
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-L', '--logical')
+	parser.add_argument('-P', '--physical')
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+	# parser.add_argument()
+
+	args = parser.parse_args()
+	print args
