da3cab09512ae2ae9c43026a2be798bafed7a34a
Carmine <cjlettie@asu.edu>
2016-04-03 11:02:26 -0700

    Added to mac_conversion.py
    Add a few argument parsers to mac_conversion

---
+++ mac_conversion.py
@@ -0,0 +1,8 @@
+import os
+#cmd = []
+if __name__ = "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-T')
+	parser.add_argument('-D')
+	parser.add_argument('-f')
+	parser.add_argument('-h')
