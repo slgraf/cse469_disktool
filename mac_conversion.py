cd0e66f2849f90fd29b94f87fbfa2e9c85b82488
Carmine <cjlettie@asu.edu>
2016-04-03 11:21:40 -0700

    mac_conversion update
    added a hex to binary converter

--- mac_conversion.py
+++ mac_conversion.py
@@ -1,8 +1,21 @@
 import os
+import argparse
+import datetime as dt
 #cmd = []
+
 if __name__ = "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-T')
 	parser.add_argument('-D')
 	parser.add_argument('-f')
 	parser.add_argument('-h')
+
+	args = parse.parse_args()
+	args.
+
+#converting hex to date?
+my_hex = "4f42"
+scale = 16
+num_of_bits = 8
+binary = bin(int(my_hex, scale))[2:].zfill(num_of_bits)
+print binary
