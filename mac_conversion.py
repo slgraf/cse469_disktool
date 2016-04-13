ef2ee1a5e1f3fdcaae05a0c39f137ff48dcc0e70
Carmine <cjlettie@asu.edu>
2016-04-12 20:09:24 -0700

    Finish mac_conversion (I hope)
    Mac_conversion takes in both file names and hex inputs and converts them
    to either date or time

--- mac_conversion.py
+++ mac_conversion.py
@@ -1,6 +1,5 @@
 import os
-import argparse
-import datetime as dt
+import sys
 
 months = { 
 1: "Jan",
@@ -17,55 +16,63 @@ months = {
 12: "Dec"
 }
 
-#import binascci
-#cmd = []
-
-#if __name__ = "__main__":
-#	parser = argparse.ArgumentParser()
-#	parser.add_argument('-T')
-#	parser.add_argument('-D')
-#	parser.add_argument('-f')
-#	parser.add_argument('-h')
-#
-#	args = parse.parse_args()
-#	args.
-#
-#converting hex to date?
-
 #converts hex to date, need to find the format for taking in inputs
 #this input comes in from the command line
-#def hex_to_bin(filename):
-my_hex = "3a81"
-scale = 16
-num_of_bits = len(my_hex)*4
-binary_rep = ( bin(int(my_hex, scale))[2:] ).zfill(num_of_bits)
+def hex_to_date(hex1):
+	my_hex = hex1[2:len(hex1)]
+	scale = 16
+	num_of_bits = len(my_hex)*4
+	binary_rep = ( bin(int(my_hex, scale))[2:] ).zfill(num_of_bits)
 
-year = binary_rep[0:7]
-month = binary_rep[7:11]
-day = binary_rep[11:16]
+	year = binary_rep[0:7]
+	month = binary_rep[7:11]
+	day = binary_rep[11:16]
 
-int_year = 1980 + int(year, 2)
-int_month = int(month, 2)
-int_day = int(day, 2)
+	int_year = 1980 + int(year, 2)
+	int_month = int(month, 2)
+	int_day = int(day, 2)
 
-print ("Date: ",months[int_month]," ",int_day,", ",int_year)
+	print ("Date: ",months[int_month]," ",int_day,", ",int_year)
 
 #moving on to the conversion to time from hex
 #this input comes in from a .txt file
-my_hex2 = "53f6"
-num_of_bits2 = len(my_hex2)*4
-binary_rep2 = ( bin(int(my_hex2, scale))[2:]).zfill(num_of_bits2)
+def hex_to_time(hex2):
+	my_hex2 = hex2[2:len(hex2)]
+	scale = 16
+	num_of_bits2 = len(my_hex2)*4
+	binary_rep2 = ( bin(int(my_hex2, scale))[2:]).zfill(num_of_bits2)
 
-hour = binary_rep2[0:5]
-minute = binary_rep2[5:11]
-second = binary_rep2[11:16]
+	hour = binary_rep2[0:5]
+	minute = binary_rep2[5:11]
+	second = binary_rep2[11:16]
 
-int_hour = int(hour, 2)
-int_minute = int(minute, 2)
-int_second = 2*int(second, 2)
+	int_hour = int(hour, 2)
+	int_minute = int(minute, 2)
+	int_second = 2*int(second, 2)
 
-if int_hour>12:
-	int_hour = int_hour - 12
-	print("Time: ", int_hour, ":", int_minute, ":", int_second, "PM")
-else:
-	print("Time: ", int_hour, ":", int_minute, ":", int_second, "AM")
+	if int_hour>12:
+		int_hour = int_hour - 12
+		print("Time: ", int_hour, ":", int_minute, ":", int_second, "PM")
+	else:
+		print("Time: ", int_hour, ":", int_minute, ":", int_second, "AM")
+
+#hex_to_time("0x53f6")
+#hex_to_date("0x3a81")
+
+#main code
+list_of_arg = (sys.argv)
+
+if list_of_arg[1] == "-T":
+	if list_of_arg[2] == "-h":
+		hex_to_time(list_of_arg[3])
+	elif list_of_arg[2] == "-f":
+		f = open(list_of_arg[3], 'r')
+		my_hex3 = f.read()
+		hex_to_time(my_hex3)
+elif list_of_arg[1] == "-D":
+	if list_of_arg[2] == "-h":
+		hex_to_date(list_of_arg[3])
+	elif list_of_arg[2] == "-f":
+		f = open(list_of_arg[3], 'r')
+		my_hex3 = f.read()
+		hex_to_time(my_hex3)
