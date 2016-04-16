4aaba23abb283e7b4dfa42e39760f4a17678162e
slgraf <slgraf@asu.edu>
2016-04-16 16:15:21 -0700

    Adding stuff

--- address4forensics.py
+++ address4forensics.py
@@ -1,25 +1,21 @@
-# project imports
-import argparse
-import os
+"""
+Usage:
+	address4forensics.py <files>
+Options:
+	-L --logical	Calculate the logical address from either
+			the cluster address or the physical address.
+			Either -c or -p must be given.
+	-P --physical	Calculate the physical address from either
+			the cluster address or the physical address.
+			Either -c or -l must be given.
+	-C --cluster	Calculate the cluster address from either
+			the cluster address or the physical address.
+			Either -l or -p must be given.
+	-b OFFSET --partition-start=OFFSET
+"""
+import docopt
 
-# cmd=[]
 
-# put at end of code
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-L', '--logical')
-	parser.add_argument('-P', '--physical')
-	parser.add_argument('-C', '--cluster')
-	# parser.add_argument()
-	# parser.add_argument()
-	# parser.add_argument()
-	# parser.add_argument()
-	# parser.add_argument()
-	# parser.add_argument()
-	# parser.add_argument()
-	# parser.add_argument()
-	# parser.add_argument()
-	# parser.add_argument()
-
-	args = parser.parse_args()
-	print args
+if __name__ == '__main__':
+	arguments = docopt.docopt(__doc__)
+	print arguments
