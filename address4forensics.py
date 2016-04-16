dd2b9df85c553e4fc1503152149aa1fd53eaa5dd
slgraf <slgraf@asu.edu>
2016-04-16 16:59:53 -0700

    Arguments stuff

--- address4forensics.py
+++ address4forensics.py
@@ -1,21 +1,31 @@
-"""
-Usage:
-	address4forensics.py <files>
+"""Usage: address4forensics.py  -L|-P|-C [-b offset] [-B [-s bytes]] [-l address] [-p address] 
+	[-c address -k sectors -r sectors -t tables -f sectors]
+	
 Options:
-	-L --logical	Calculate the logical address from either
+	-L, --logical	Calculate the logical address from either
 			the cluster address or the physical address.
 			Either -c or -p must be given.
-	-P --physical	Calculate the physical address from either
+	-P, --physical	Calculate the physical address from either
 			the cluster address or the physical address.
 			Either -c or -l must be given.
-	-C --cluster	Calculate the cluster address from either
+	-C, --cluster	Calculate the cluster address from either
 			the cluster address or the physical address.
 			Either -l or -p must be given.
-	-b OFFSET --partition-start=OFFSET
+	-b offset, --partition-start=offset
+	-B, --byte-address
+	-s bytes, --sector-size=bytes
+	-l address, --logical-known=address
+	-p address, --physical-known=address
+	-c address, --cluster-known=address
+	-k sectors, --cluster-size=sectors
+	-r sectors, --reserved=sectors
+	-t tables, --fat-tables=tables
+	-f sectors, --fat-length=sectors
 """
 import docopt
 
 
 if __name__ == '__main__':
 	arguments = docopt.docopt(__doc__)
-	print arguments
+	offset = int(arguments['--partition-start'])
+	print offset
