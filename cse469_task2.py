889eabae59b315bdd1cc7ff6a6b429d88c6fb960
Carmine <cjlettie@asu.edu>
2016-04-17 16:27:56 -0700

    added partition types we will be using
    added the partition types from Notes 7 as the code originally had too
    many

--- cse469_task2.py
+++ cse469_task2.py
@@ -3,6 +3,37 @@ import getopt, sys
 import hashlib
 import struct
 
+PartitionTypes = { 
+    0x00:"Empty",
+    0x01:"DOS 12-bit FAT",
+    0x04:"DOS 16-bit FAT for partitions larger than 32 MB",
+    0x05:"Extended partition",
+    0x06:"DOS 16-bit FAT for partitions larger than 32 MB",
+    0x07:"NTFS",
+    0x08:"AIX bootable partition",
+    0x09:"AIX data partition",
+    0x0b:"DOS 32-bit FAT",
+    0x0c:"DOS 32-bit FAT for 13 support",
+    0x17:"Hidden NTFS partition",
+    0x1b:"Hidden FAT32 partition",
+    0x1e:"Hidden VFAT partition",
+    0x3c:"Partition Magic recovery partition",
+    0x66:"Novell partition",
+    0x67:"Novell partition",
+    0x68:"Novell partition",
+    0x69:"Novell partition",
+    0x81:"Linux",
+    0x82:"Solaris x86 or Linux Swap",
+    0x83:"Linux native file system",
+    0x86:"FAT16 volum/stripe set (Windows NT)",
+    0x87:"High Performance File System",
+    0xa5:"FreeBSD and BSD/386",
+    0xa6:"OpenBSD",
+    0xa9:"NetBSD",
+    0xc7:"Corrupted NTFS volume/stripe set"
+    0xeb:"BeOS"
+}
+
 class PartitionEntry:
     def __init__(self, data):
         self.BootableFlag = struct.unpack("<c", data[:1])[0]
