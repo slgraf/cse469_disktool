5eca0aeef759119c3bcace3d19f425eae0f0165a
Carmine <cjlettie@asu.edu>
2016-04-16 17:56:25 -0700

    address4forensics with -L inputs (hopefully) done
    Finished the --logical or -L inputs, but some stuff still needs to be
    tested to make sure that it's working.  The inputs in the usage are very
    long and should be checked to make sure they work.

--- address4forensics.py
+++ address4forensics.py
@@ -1,12 +1,58 @@
-"""Usage: address4forensics.py  --partition-start=offset
+"""Usage: address4forensics.py  -L | --logical [-b OFFSET | --partition-start=offset] 
+			 [[-p ADDRESS | --physical-known=address] | [[-c ADDRESS | --cluster-known=address] [
+			 -k SECTORS | --cluster-size=sectors] [-r SECTORS | --reserved=sectors] [
+			 -t TABLES | --fat-tables=tables] [-f SECTORS | --fat-length=sectors]]]
+		  address4forensics.py  -P | --physical [-b OFFSET | --partition-start=offset]
+		  	 [[-l ADDRESS | --logical-known=address] | [[-c ADDRESS | --clusterknown=address] [
+			 -k SECTORS | --cluster-size=sectors] [-r SECTORS | --reserved=sectors] [
+			 -t TABLES | --fat-tables=tables] [-f SECTORS | --fat-length=sectors]]]
+		  address4forensics.py  -C | --cluster [-b OFFSET | --partition-start=offset]
+		  	 [[-l ADDRESS | --logical-known=address] | [-p ADDRESS | --physical-known=address]]
 
 Options:
-	--partition-start=offset
+	-L, --logical	:Calculate the logical address from either
+			the cluster address or the physical address.
+			Either -c or -p must be given.
+	-P, --physical	:Calculate the physical address from either
+			the cluster address or the physical address.
+			Either -c or -l must be given.
+	-C, --cluster	:Calculate the cluster address from either
+			the cluster address or the physical address.
+			Either -l or -p must be given.
+	-b --partition-start=offset
+	-B, --byte-address
+	-s bytes, --sector-size=bytes
+	-l address, --logical-known=address
+	-p address, --physical-known=address
+	-c address, --cluster-known=address
+	-k sectors, --cluster-size=sectors
+	-r sectors, --reserved=sectors
+	-t tables, --fat-tables=tables
+	-f sectors, --fat-length=sectors
+
 """
 import docopt
 
 
 if __name__ == '__main__':
 	arguments = docopt.docopt(__doc__)
-	offset = int(arguments['--partition-start'])
-	print offset
+	if arguments["--logical"]:
+		if arguments['-partition-start']:
+			offset = int(arguments['--partition-start'])
+			if arguments['--physical-known']:
+				phy_address = int(arguments['--physical-known'])
+			elif arguments['-cluster-known']:
+				clus_address = int(arguments['--cluster-known'])
+				if arguments['--cluster-size']:
+					sec_per_clus = int(arguments['--cluster-size'])
+					if arguments['--reserved']:
+						reser_sec = int(arguments['-reserved'])
+						if arguments['--fat-tables']:
+							FAT_tables = int(arguments['-fat-tables'])
+							if arguments['--fat-length']
+								len_of_FAT = int(arguments['--fat-length'])
+
+
+	#if arguments["-P"]
+
+	#address4forensics.py  --logical [-b OFFSET | --partition-start=offset]
