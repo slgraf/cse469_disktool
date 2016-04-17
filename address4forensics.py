20375a9a22d0d311d16afed2e1fdff04b928bc61
slgraf <slgraf@asu.edu>
2016-04-17 15:50:30 -0700

    getting args working

--- address4forensics.py
+++ address4forensics.py
@@ -1,39 +1,29 @@
-"""Usage: address4forensics.py  -L|-P|-C [-b offset] [-B [-s bytes]] [-l address] [-p address] 
-	[-c address -k sectors -r sectors -t tables -f sectors]
+"""Usage: address4forensics.py  -L [-p address] [-c address]
 	
-Options:
-	-L, --logical	Calculate the logical address from either
-			the cluster address or the physical address.
-			Either -c or -p must be given.
-	-P, --physical	Calculate the physical address from either
-			the cluster address or the physical address.
-			Either -c or -l must be given.
-	-C, --cluster	Calculate the cluster address from either
-			the cluster address or the physical address.
-			Either -l or -p must be given.
-	-b offset, --partition-start=offset
-	-B, --byte-address
-	-s bytes, --sector-size=bytes
-	-l address, --logical-known=address
-	-p address, --physical-known=address
-	-c address, --cluster-known=address
-	-k sectors, --cluster-size=sectors
-	-r sectors, --reserved=sectors
-	-t tables, --fat-tables=tables
-	-f sectors, --fat-length=sectors
+-L, --logical
+-P, --physical
+-C, --cluster
+-b offset, --partition-start=offset
+-B, --byte-address
+-s bytes, --sector-size=bytes
+-l address, --logical-known=address
+-p address, --physical-known=address
+-c address, --cluster-known=address
+-k sectors, --cluster-size=sectors
+-r sectors, --reserved=sectors
+-t tables, --fat-tables=tables
+-f sectors, --fat-length=sectors
 """
 import docopt
 
 debug=True
 
 def cmd_logical():
-	if arguments['-p'] or arguments ['--physical-known']:
+	if arguments['--physical-known']:
+		print '_physical-known'
+		print int(arguments['--partition-known'])
+	elif arguments['--cluster-known'] and arguments['--cluster-size'] and arguments['--reserved'] and arguments['--fat-tables'] and arguments['--fat-length']:
 		pass
-	elif arguments['-c'] and arguments['-k'] and arguments['-r'] and arguments['-t'] and arguments['-f']:
-		pass
-
-
-
 
 
 
@@ -41,6 +31,33 @@ if __name__ == '__main__':
 	arguments = docopt.docopt(__doc__)
 	if debug:
 		print 'arguments:\n{0}'.format(arguments)
-	if arguments['-L']:
-		if arguments['-c'] or arguments['-p']:
+	if arguments['--logical']:
+		print '_logical'
+		if arguments['--cluster-known'] or arguments['--physical-known']:
+			print '_cmd_logical'
 			cmd_logical()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
