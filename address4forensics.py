5d88aea23f2108d5eba21715d53398c791a3e7f3
slgraf <slgraf@asu.edu>
2016-04-18 11:34:14 -0700

    Made progress on functions

--- address4forensics.py
+++ address4forensics.py
@@ -21,15 +21,38 @@ Usage: 	address4forensics.py  -L [-b offset] [-B [-s bytes]] [-p address] [-c ad
 import docopt
 
 debug=True
-
+# how does --byte-address change things?
 def cmd_logical():
 	if arguments['--physical-known']:
 		print '_physical-known'
-		print int(arguments['--physical-known'])
-		print
+		if arguments['--partition-start'] is not None:
+			address=int(arguments['--physical-known'])
+			offset=int(arguments['--partition-start'])
+			address-=offset
+			return address
+		else:
+			return int(arguments['--physical-known'])
 	elif arguments['--cluster-known'] and arguments['--cluster-size'] and arguments['--reserved'] and arguments['--fat-tables'] and arguments['--fat-length']:
+		# use CHS and what not to calc
 		pass
 
+def cmd_physical():
+	if arguments['--logical-known']:
+		address=int(arguments['--logical-known'])
+		offset=0
+		if arguments['--partition-start'] is not None:
+			offset=int(arguments['--partition-start'])
+		return address+offset
+	elif arguments['--cluster-known'] and arguments['--cluster-size'] and arguments['--reserved'] and arguments['--fat-tables'] and arguments['--fat-length']:
+		# use CHS and what not to calc
+		pass
+	
+
+def cmd_cluster():
+	if arguments['--physical-known']:
+		p_address=int(arguments['--physical-known'])
+	elif arguments['logical-known']:
+		l_address=int(arguments['--logical-known'])
 
 
 if __name__ == '__main__':
@@ -41,6 +64,14 @@ if __name__ == '__main__':
 		if arguments['--cluster-known'] or arguments['--physical-known']:
 			print '_cmd_logical'
 			cmd_logical()
+	elif arguments['--cluster']:
+		print '_cluster'
+		if arguments['--logical-known'] or arguments['--physical-known']:
+			cmd_cluster()
+	elif arguments['--physical']:
+		print '_physical'
+		if arguments['--logical-known'] or arguments['--cluster-known']:
+			cmd_physical()
 
 
 
