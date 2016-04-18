ea2923ebc9ca048cb5765d465a2538d9ea0ab023
Carmine <cjlettie@asu.edu>
2016-04-17 17:07:25 -0700

    added functions that compute the size and starting sectors
    added 3 functions, one that creates a partition table, one that finds
    the starting sector of each partition and one that finds the size of the
    partition

--- cse469_task2.py
+++ cse469_task2.py
@@ -34,29 +34,36 @@ PartitionTypes = {
     0xeb:"BeOS"
 }
 
-class PartitionEntry:
-    def __init__(self, data):
-        self.BootableFlag = struct.unpack("<c", data[:1])[0]
-        self.StartCHS0 = struct.unpack("<B", data[1:2])[0]
-        self.StartCHS1 = struct.unpack("<B", data[2:3])[0]
-        self.StartCHS2 = struct.unpack("<B", data[3:4])[0]
-        self.PartitionType = struct.unpack("<c", data[4:5])[0]
-        self.EndCHS0 = struct.unpack("<B", data[5:6])[0]
-        self.EndCHS1 = struct.unpack("<B", data[6:7])[0]
-        self.EndCHS2 = struct.unpack("<B", data[7:8])[0]
-        self.StartLBA = struct.unpack("<I", data[8:12])[0]
-        self.SizeInSectors = struct.unpack("<i", data[12:16])[0]
+def PartitionEntry(data):   
+    state_of_partition = data[:1]
+    beg_head = data[1:2]
+    beg_sec = data[2:4]
+    type_par = data[4:5]
+    end_head = data[5:6]
+    end_sec = data[6:8]
+    num_sec_MBR = data[8:12]
+    num_sec_par = data[12:16]
 
-class PartitionTable:
-    def __init__(self, data):
-        self.DiskSignature0 = struct.unpack("<B", data[:1])[0]
-        self.DiskSignature1 = struct.unpack("<B", data[1:2])[0]
-        self.DiskSignature2 = struct.unpack("<B", data[2:3])[0]
-        self.DiskSignature3 = struct.unpack("<B", data[3:4])[0]
-        self.Unused = struct.unpack("<H", data[4:6])[0]
-        self.Entry0 = PartitionEntry(data[6:22])
-        self.Entry1 = PartitionEntry(data[22:38])
-        self.Entry2 = PartitionEntry(data[38:54])
-        self.Entry3 = PartitionEntry(data[54:70])
-        self.Signature = struct.unpack("<H", data[70:72])[0]
+def starting_sec(data):
+    hex1 = data[:2]
+    hex2 = data[2:4]
+    hex3 = data[4:6]
+    hex4 = data[6:8]
+    final_hex = hex4+hex3+hex2+hex1
+    scale = 16
+    num_of_bits2 = len(final_hex)*4
+    binary_rep2 = ( bin(int(final_hex, scale))[2:]).zfill(num_of_bits2)
+    starting_sector = int(binary_rep2, 2)
+
+def size_of_par(data):
+    hex1 = data[:2]
+    hex2 = data[2:4]
+    hex3 = data[4:6]
+    hex4 = data[6:8]
+    final_hex = hex4+hex3+hex2+hex1
+    scale = 16
+    num_of_bits2 = len(final_hex)*4
+    binary_rep2 = ( bin(int(final_hex, scale))[2:]).zfill(num_of_bits2)
+    size_par = int(binary_rep2, 2)
+
 
