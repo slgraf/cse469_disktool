70418a5b3168a18827fd88deb40eb590cdd2aa6e
slgraf <slgraf@asu.edu>
2016-04-19 15:21:12 -0700

    Finished cmd_cluster.

--- address4forensics.py
+++ address4forensics.py
@@ -39,20 +39,21 @@ def cmd_logical():
 
 	elif arguments['--cluster-known'] and arguments['--cluster-size'] and arguments['--reserved'] and arguments['--fat-tables'] and arguments['--fat-length']:
 		# use CHS and what not to calc
-		print '_cluster-known'
-		c_address = int(arguments['--cluster-known'])
-		c_sz = int(arguments['--cluster-size'])
-		reserved_sec = int(arguments['--reserved'])
-		fat_tables = int(arguments['--fat-tables'])
-		fat_length = int(arguments['--fat-length'])
+		# print '_cluster-known'
+		# c_address = int(arguments['--cluster-known'])
+		# c_sz = int(arguments['--cluster-size'])
+		# reserved_sec = int(arguments['--reserved'])
+		# fat_tables = int(arguments['--fat-tables'])
+		# fat_length = int(arguments['--fat-length'])
 
-		address = c_address - (c_sz*reserved_sec*fat_tables*fat_length)
-		if arguments['--byte-address']:
-			sector_sz = 512
-			if arguments['--sector-size'] is not None:
-				sector_sz = int(arguments['--sector-size'])
-			return address*sector_sz
-		return address
+		# address = c_address - (c_sz*reserved_sec*fat_tables*fat_length)
+		# if arguments['--byte-address']:
+		# 	sector_sz = 512
+		# 	if arguments['--sector-size'] is not None:
+		# 		sector_sz = int(arguments['--sector-size'])
+		# 	return address*sector_sz
+		# return address
+		pass
 
 def cmd_physical():
 	if arguments['--logical-known']:
@@ -61,6 +62,11 @@ def cmd_physical():
 		offset=0
 		if arguments['--partition-start'] is not None:
 			offset=int(arguments['--partition-start'])
+		if arguments['--byte-address']:
+			sector_sz = 512
+			if arguments['--sector-size'] is not None:
+				sector_sz = int(arguments['--sector-size'])
+			return sector_sz*(p_address-offset)
 		return address+offset
 	elif arguments['--cluster-known'] and arguments['--cluster-size'] and arguments['--reserved'] and arguments['--fat-tables'] and arguments['--fat-length']:
 		# use CHS and what not to calc
@@ -69,12 +75,19 @@ def cmd_physical():
 	
 
 def cmd_cluster():
+	address = None
+	sector_sz = 1
 	if arguments['--physical-known']:
 		print '_physical-known'
-		p_address=int(arguments['--physical-known'])
+		address=int(arguments['--physical-known'])
 	elif arguments['logical-known']:
 		print '_logical-known'
-		l_address=int(arguments['--logical-known'])
+		address=int(arguments['--logical-known'])
+	if arguments['--byte-address']:
+		sector_sz = 512
+		if arguments['--sector-size']:
+			sector_sz = int(arguments['--sector-size'])
+	return address*sector_sz
 
 
 if __name__ == '__main__':
