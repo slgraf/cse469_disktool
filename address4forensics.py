5c6a316894889d6044388cecc4da02b24f689d20
slgraf <slgraf@asu.edu>
2016-04-19 16:06:20 -0700

    Removed debug statements

--- address4forensics.py
+++ address4forensics.py
@@ -22,7 +22,6 @@ import docopt
 
 def cmd_logical():
 	if arguments['--physical-known']:
-		print '_physical-known'
 		p_address = int(arguments['--physical-known'])
 		offset = 0
 		if arguments['--partition-start'] is not None:
@@ -35,8 +34,6 @@ def cmd_logical():
 		return p_address-offset
 
 	elif arguments['--cluster-known'] and arguments['--cluster-size'] and arguments['--reserved'] and arguments['--fat-tables'] and arguments['--fat-length']:
-		# use CHS and what not to calc
-		print '_cluster-known'
 		c_address = int(arguments['--cluster-known'])
 		c_sz = int(arguments['--cluster-size'])
 		reserved_sec = int(arguments['--reserved'])
@@ -53,7 +50,6 @@ def cmd_logical():
 
 def cmd_physical():
 	if arguments['--logical-known']:
-		print '_logical-known'
 		address=int(arguments['--logical-known'])
 		offset=0
 		if arguments['--partition-start'] is not None:
@@ -66,8 +62,6 @@ def cmd_physical():
 		return address+offset
 
 	elif arguments['--cluster-known'] and arguments['--cluster-size'] and arguments['--reserved'] and arguments['--fat-tables'] and arguments['--fat-length']:
-		# use CHS and what not to calc
-		print '_cluster-known'
 		c_address = int(arguments['--cluster-known'])
 		c_sz = int(arguments['--cluster-size'])
 		reserved_sec = int(arguments['--reserved'])
@@ -86,10 +80,8 @@ def cmd_cluster():
 	address = None
 	sector_sz = 1
 	if arguments['--physical-known']:
-		print '_physical-known'
 		address=int(arguments['--physical-known'])
 	elif arguments['logical-known']:
-		print '_logical-known'
 		address=int(arguments['--logical-known'])
 	if arguments['--byte-address']:
 		sector_sz = 512
@@ -101,17 +93,16 @@ def cmd_cluster():
 if __name__ == '__main__':
 	arguments = docopt.docopt(__doc__)
 
+	resp = -1
 	if arguments['--logical']:
-		print '_logical'
 		if arguments['--cluster-known'] or arguments['--physical-known']:
-			print '_cmd_logical'
-			cmd_logical()
+			resp = cmd_logical()
 	elif arguments['--cluster']:
-		print '_cluster'
 		if arguments['--logical-known'] or arguments['--physical-known']:
-			cmd_cluster()
+			resp = cmd_cluster()
 	elif arguments['--physical']:
-		print '_physical'
 		if arguments['--logical-known'] or arguments['--cluster-known']:
-			cmd_physical()
+			resp = cmd_physical()
+
+	print resp
 
